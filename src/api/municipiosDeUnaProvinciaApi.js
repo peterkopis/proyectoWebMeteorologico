275afe1ea144d3542ffcb24ab8768d5e1c527e07
Peter <peter.pis@centrum.sk>
2023-10-01 14:39:42 +0200

    continue with developing of method municipios de una provincia --sige desarrollando el metodo municipios de una provincia

--- src/api/municipiosDeUnaProvinciaApi.js
+++ src/api/municipiosDeUnaProvinciaApi.js
@@ -3,9 +3,24 @@
 let obtenerJsonDeApi = require('./obtenerJsonDeApi')
 
 module.exports = function municipiosDeUnaProvincia(urlcodProv){
+    let elementoMunicipio;
+    let menuDeMunicipios = document.getElementById('exampleFormControlSelect1')
 
     const urlPrefix = 'https://www.el-tiempo.net/api/json/v2/provincias/'
     const urlPostfix = '/municipios'
     let urlMunicipiosDeUnaProvincia = urlPrefix+urlcodProv+urlPostfix
-    console.log(urlMunicipiosDeUnaProvincia)
+    //console.log(urlMunicipiosDeUnaProvincia)
+    obtenerJsonDeApi(urlMunicipiosDeUnaProvincia).then(json => {
+
+        //cuanto se elige una nueva provincia los municipios se cargan de nuevo 
+        menuDeMunicipios.innerHTML = ''
+        for(let municipio  of json.municipios){
+
+            //console.log(municipio)
+            elementoMunicipio =document.createElement('option')
+            elementoMunicipio.innerText = municipio.NOMBRE
+            menuDeMunicipios.appendChild(elementoMunicipio)
+
+
+        }})
 }
